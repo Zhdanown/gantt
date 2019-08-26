60328f0e246f4aa0339c50781c87ad1a40f71001
Nikolay Zhdanow <zhdanown@gmail.com>
2019-08-26 14:01:20 +0300

    init commit

---
+++ src/components/Diagramm.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Diagramm() {
+  return (
+    <div className="diagramm">
+      <section className="legend">Legend</section>
+      <section className="chart">
+        <div className="dates">Dates</div>
+        <div className="data">Data</div>
+      </section>
+    </div>
+  );
+}
+
+export default Diagramm;
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "../AT_logo.png";
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="nav-wrapper container">
+        <a href="#" className="brand-logo">
+          <img src={logo} className="App-logo" alt="logo" />
+        </a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <a href="sass.html">Техника</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
